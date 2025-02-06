1b51468517084cc1f3517a142d1cf1dea3b6fd02
Elayabarathi <elayabarathiedison@gmail.com>
2025-02-06 16:21:24 +0530

    refining code with list comprehension

--- main.py
+++ main.py
@@ -16,12 +16,10 @@ while len(guessed_state) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_state)}/50 States Correct", prompt="Guess a state's name?").title()
 
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_state:
-            if state not in guessed_state:
-                missing_states.append(state)
+        missing_states = [state for state in all_state if state not in guessed_state]
         print(f"You guessed {len(guessed_state)}/50 states correctly!")
-        print(f"States to Learn: {', '.join(missing_states)}")
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
         break
 
     if answer_state in all_state:
